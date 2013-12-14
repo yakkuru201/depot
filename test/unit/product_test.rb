013a0ff143cbcc9b1e350b0ac155958886e1747b
yakkuru201 <tonamimagic@gmail.com>
2013-12-14 22:01:27 +0900

    タスクB終了

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new(
-      title: "My book Title",
+      title: "products(:ruby).title",
       description: "yyy",
       price: 1,
-      image_url: image_url
+      image_url: "fred.gif"
     )
+    assert !product.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join(';')
   end
 
 test "image url"
