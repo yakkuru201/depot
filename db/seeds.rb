01efefff8ffa970928d6566c45f1aa19dc371b9b
yakkuru201 <tonamimagic@gmail.com>
2013-12-14 18:18:09 +0900

    他の２つの商品を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,18 @@
 
 Product.delete_all
 
+Product.create(title: 'CoffeeScript',
+  description: 
+    %{<p>
+        CoffeeScript is JavaScript done right. It provides all of JavaScript's
+	functionality wrapped in a cleaner, more succinct syntax. In the first
+	book on this exciting new language, CoffeeScript guru Trevor Burnham
+	shows you how to hold onto all the power and flexibility of JavaScript
+	while writing clearer, cleaner, and safer code.
+      </p>},
+  image_url:   'cs.jpg',    
+  price: 36.00)
+# . . .
 Product.create(title: 'Programming Ruby 1.9',
   description:
     %{<p>
@@ -17,3 +29,17 @@ Product.create(title: 'Programming Ruby 1.9',
       </p>},
   image_url: 'ruby.jpg',
   price: 49.95)
+# . . .
+
+Product.create(title: 'Rails Test Prescriptions',
+  description: 
+    %{<p>
+        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
+        Rails applications, covering Test-Driven Development from both a
+        theoretical perspective (why to test) and from a practical perspective
+        (how to test effectively). It covers the core Rails testing tools and
+        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
+        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
+      </p>},
+  image_url: 'rtp.jpg',
+  price: 34.95)
