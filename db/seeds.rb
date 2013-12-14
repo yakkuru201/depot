aeb68ca72500a8c7cb93fd67d3bc2d476b1844b6
yakkuru201 <tonamimagic@gmail.com>
2013-12-14 16:28:30 +0900

    seedsの書き忘れを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Product.delete_all
+
 Product.create(title: 'Programming Ruby 1.9',
   description:
     %{<p>
