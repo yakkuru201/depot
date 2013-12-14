eb8a411ce61b531395ef54ed03fbc6a22f8b7fbe
yakkuru201 <tonamimagic@gmail.com>
2013-12-14 21:01:44 +0900

    アサーションの設定

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+test "product attributes must not be empty" do
+	product = Product.new(
+		title: 			"My book Title",
+		description: 	"yyy",
+        image_url: 		"zzz.jpg"
+    )
+
+    product.price = -1
+	assert product.invalid?
+	assert_equal "must be greater than or equal to 0.01",
+		product.errors[price].join(';')
+
+	product.price = 0
+	assert product.invalid?
+	assert_equal "must be greater than or equal to 0.01",
+		product.errors[:price].join(';')
+
+	product.price = 1
+	assert product.valid?
 end
+
